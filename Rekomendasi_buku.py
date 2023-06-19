86caa8c717056c13149a017cdd5b70bebde4abe7
Diopranata01 <bagusdio01@gmail.com>
2023-06-19 16:43:11 +0800

    new

--- Rekomendasi_buku.py
+++ Rekomendasi_buku.py
@@ -9,7 +9,7 @@ from keras.models import load_model
 
 
 # Memuat model
-model = tf.keras.models.load_model('save weight', compile=False)
+model = tf.keras.models.load_model('model.h5', compile=False)
 
 
 Dataset_buku = './data_sets'
